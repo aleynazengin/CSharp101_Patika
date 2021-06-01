046140af3203dbf8017ae0922a843238c76bf594
Aleyna Zengin <aleystyles23@gmail.com>
2021-06-02 01:44:09 +0300

    Konsol Tip Dönüşümleri Çalışması

--- Calisma2/Program.cs
+++ Calisma2/Program.cs
@@ -35,6 +35,51 @@ namespace Calisma2
             DateTime dt= DateTime.Now;
             Console.WriteLine(dt);
 
+            object o1 = "x";
+            object o2= 'y';
+            object o3 = 4;
+            object o4 = 4.3;
+
+            //string ifadeler
+
+            string str1= string.Empty;
+            str1 = "Aleyna Zengin";
+            string ad = "Aleyna";
+            string soyad = "Zengin";
+            string tamIsim = ad + " " + soyad;
+
+            //Integer tanımlama şekilleri
+
+            int integer1= 5;
+            int integer2= 3;
+            int integer3= integer1+integer2;
+
+            //boolean
+            bool bool1 = 10>2;
+
+            //Değişken dönüşümleri
+            string str20 = "20";
+            int int20 = 20;
+            string yeniDeger = str20 + int20.ToString();
+            Console.WriteLine(yeniDeger); // Çıktısı 2020
+
+            int int21= int20 + Convert.ToInt32(str20);
+            Console.WriteLine(int21); // Çıktısı 40
+
+            int int22= int20 + int.Parse(str20); //Çıktısı 40
+
+            // datetime
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(datetime);
+
+            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(datetime2);
+
+            //saat
+            string hour = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(hour);
+
 
 
         }
